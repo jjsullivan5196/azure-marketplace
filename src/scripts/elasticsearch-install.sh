6ef1dc69d668e07ad4c5a612c1a86cb727adef4d
John Sullivan <jsullivan@csumb.edu>
2019-01-25 15:42:07 -0800

    Allow "insecure" cluster options
    I HAVE DONNITS

--- src/scripts/elasticsearch-install.sh
+++ src/scripts/elasticsearch-install.sh
@@ -1110,6 +1110,7 @@ configure_elasticsearch()
     log "[configure_elasticsearch] configure elasticsearch heap size - $ES_HEAP megabytes"
     sed -i -e "s/^\-Xmx.*/-Xmx${ES_HEAP}m/" /etc/elasticsearch/jvm.options
     sed -i -e "s/^\-Xms.*/-Xms${ES_HEAP}m/" /etc/elasticsearch/jvm.options
+    echo "-Des.allow_insecure_settings=true" >> /etc/elasticsearch/jvm.options
 }
 
 configure_os_properties()
